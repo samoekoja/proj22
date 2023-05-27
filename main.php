e9daea1d7dee1d9931378a46ce0d42c5201116b0
samoek <samuelekojao@gmail.com>
2023-05-27 14:21:15 +0100

    Update main.php

--- main.php
+++ main.php
@@ -5,12 +5,18 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Main Page</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="bootstrap3/css/bootstrap.min.css">
+    <link rel="stylesheet" href="MyStyle.css">
+    <script src="script.js"></script>
+</head>
 </head>
 <body class="jumbotron">
-    <div>
+    <div >
         <h1>Welcome User</h1>
     </div>
-    <div>
+    <div class = "col-sm-4 login">
+        <p>Your Code is </p>
         <?php
         $codeopen = rand(0000, 9999);
         
